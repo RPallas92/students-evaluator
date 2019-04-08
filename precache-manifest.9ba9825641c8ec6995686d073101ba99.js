3ca58c62eb42e30eb2b6abdf4f2e58e3d8bc47f8
Ricardo Pallás <rpallas92@gmail.com>
2019-04-08 20:15:27 +0200

    Updates

---
+++ precache-manifest.9ba9825641c8ec6995686d073101ba99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e0d3d66d4f71dae920b",
+    "url": "/students-evaluator/static/js/runtime~main.2e0d3d66.js"
+  },
+  {
+    "revision": "fd8f83172f6a55192940",
+    "url": "/students-evaluator/static/js/main.fd8f8317.chunk.js"
+  },
+  {
+    "revision": "3e49559d3f37a3d4a805",
+    "url": "/students-evaluator/static/js/2.3e49559d.chunk.js"
+  },
+  {
+    "revision": "fd8f83172f6a55192940",
+    "url": "/students-evaluator/static/css/main.86483a8c.chunk.css"
+  },
+  {
+    "revision": "3e49559d3f37a3d4a805",
+    "url": "/students-evaluator/static/css/2.f8ef439a.chunk.css"
+  },
+  {
+    "revision": "087500235e66ea05383ff03e7ea5d891",
+    "url": "/students-evaluator/index.html"
+  }
+];
