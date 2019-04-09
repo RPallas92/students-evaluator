957bd222d50a2c1421274ef5feea3ff59945a035
Ricardo Pallás <rpallas92@gmail.com>
2019-04-09 19:55:19 +0200

    Extracted config

--- src/Database.ts
+++ src/Database.ts
@@ -1,20 +1,12 @@
 import firebase from "firebase";
 import { StudentEvaluations } from "./StudentEvaluation";
+import { firebaseConfig } from "./FirebaseConfig";
 
 export class Database {
 
     private firestore: firebase.firestore.Firestore
 
-
-    constructor() {
-        const firebaseConfig = {
-            apiKey: "AIzaSyDkId_dG4QjU31XqPLpGDyWf78gYX9wwFg",
-            authDomain: "students-evaluator.firebaseapp.com",
-            databaseURL: "https://students-evaluator.firebaseio.com",
-            projectId: "students-evaluator",
-            storageBucket: "students-evaluator.appspot.com",
-            messagingSenderId: "122329597109"
-        }
+    constructor() {        
         firebase.initializeApp(firebaseConfig);
         this.firestore = firebase.firestore()
     }
